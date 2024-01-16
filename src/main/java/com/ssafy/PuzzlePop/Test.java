62c1904cd77d7814ba8ea0bc61c2d80943693abc
dlwndus0728 <dlwndus0728@ajou.ac.kr>
2024-01-16 11:01:43 +0900

    test: Add code for testing lombok

---
+++ src/main/java/com/ssafy/PuzzlePop/Test.java
@@ -0,0 +1,16 @@
+package com.ssafy.PuzzlePop;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+public class Test {
+    int num;
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.setNum(1);
+        System.out.println("test = " + test);
+    }
+}
