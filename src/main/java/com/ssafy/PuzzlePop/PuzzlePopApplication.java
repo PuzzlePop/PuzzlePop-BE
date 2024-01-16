68a49c94ab8aec5f6c1d627efdd5b6a44a615114
dlwndus0728 <dlwndus0728@ajou.ac.kr>
2024-01-16 10:56:47 +0900

    feat: SpringBoot Init

---
+++ src/main/java/com/ssafy/PuzzlePop/PuzzlePopApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.PuzzlePop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzlePopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzlePopApplication.class, args);
+	}
+
+}
