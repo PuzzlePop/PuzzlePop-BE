20741e9f3dcae83f2c3b4cf3b357ced8839f9bef
조시훈 <kepler452k@naver.com>
2024-01-19 00:41:35 +0900

    feat: Add Lombok annotation & Rename variable name

--- src/main/java/com/ssafy/puzzlepop/team/domain/Team.java
+++ src/main/java/com/ssafy/puzzlepop/team/domain/Team.java
@@ -4,14 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long game_id;
+    private Long gameId;
     private Integer assembledPieceCount;
 }
