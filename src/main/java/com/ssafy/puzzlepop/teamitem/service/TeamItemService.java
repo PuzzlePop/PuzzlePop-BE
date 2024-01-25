2276f86ee72b85086af34614e628ddd7d656e1e2
조시훈 <kepler452k@naver.com>
2024-01-25 17:25:23 +0900

    feat: Add function getTeamItemById(), getTeamItemByTeamId()

--- src/main/java/com/ssafy/puzzlepop/teamitem/service/TeamItemService.java
+++ src/main/java/com/ssafy/puzzlepop/teamitem/service/TeamItemService.java
@@ -1,6 +1,13 @@
 package com.ssafy.puzzlepop.teamitem.service;
 
+import com.ssafy.puzzlepop.teamitem.domain.TeamItemResponseDto;
+
+import java.util.List;
+
 public interface TeamItemService {
+    public TeamItemResponseDto getTeamItemById(Long id);
+    public List<TeamItemResponseDto> getTeamItemsByTeamId(Long teamId);
+
     public Long createTeamItem(Long teamId, Long itemId);
     public Long updateTeamItem(Long teamId, Long itemId);
     public Long deleteTeamItem(Long id);
