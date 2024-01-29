0cb728477268b19915a6e372b3efa1312566af9a
김다인 <frambo1se@naver.com>
2024-01-29 10:31:32 +0900

    feat: Record Entity/Dto 작성, application.properties 수정
    - org.hibernate.dialect.MySQLDialect 인식 못 하는 부분 수정

--- src/main/java/com/ssafy/puzzlepop/record/domain/Record.java
+++ src/main/java/com/ssafy/puzzlepop/record/domain/Record.java
@@ -1,4 +1,27 @@
 package com.ssafy.puzzlepop.record.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Record {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    private String userId;
+
+    @NotNull
+    private int gameId;
 }
--- src/main/java/com/ssafy/puzzlepop/record/domain/RecordDto.java
+++ src/main/java/com/ssafy/puzzlepop/record/domain/RecordDto.java
@@ -1,4 +1,15 @@
 package com.ssafy.puzzlepop.record.domain;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class RecordDto {
+
+    private int id;
+    private String userId;
+    private int gameId;
 }
