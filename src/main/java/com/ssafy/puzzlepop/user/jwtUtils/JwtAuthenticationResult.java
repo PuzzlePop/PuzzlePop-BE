499bfe25d9e7b73a2af07185b4d8c86786072a24
김한중 <hjhzkc@gmail.com>
2024-01-31 15:33:53 +0900

    feat: JWT Filter, AuthenticationResult 추가

---
+++ src/main/java/com/ssafy/puzzlepop/user/jwtUtils/JwtAuthenticationResult.java
@@ -0,0 +1,42 @@
+package com.ssafy.puzzlepop.user.jwtUtils;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class JwtAuthenticationResult extends AbstractAuthenticationToken {
+
+    private Object principal;
+    private final String uid;
+    private final String provider;
+    private final String email;
+
+    /**
+     * Creates a token with the supplied array of authorities.
+     *
+     * @param authorities the collection of <tt>GrantedAuthority</tt>s for the principal
+     *                    represented by this authentication object.
+     */
+    public JwtAuthenticationResult(String uid, String provider, String email, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.uid = uid;
+        this.provider = provider;
+        this.email = email;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return principal;
+    }
+
+
+    public void setPrincipal(Object principal) {
+        this.principal = principal;
+    }
+}
