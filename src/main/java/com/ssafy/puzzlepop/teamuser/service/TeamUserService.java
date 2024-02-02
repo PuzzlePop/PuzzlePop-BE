1e802ddaf34679eac1b45f101df1646fa371baa9
조시훈 <kepler452k@naver.com>
2024-02-02 17:44:58 +0900

    feat: Add find functions

--- src/main/java/com/ssafy/puzzlepop/teamuser/service/TeamUserService.java
+++ src/main/java/com/ssafy/puzzlepop/teamuser/service/TeamUserService.java
@@ -1,11 +1,18 @@
 package com.ssafy.puzzlepop.teamuser.service;
 
+import com.ssafy.puzzlepop.team.domain.TeamDto;
+import com.ssafy.puzzlepop.teamuser.domain.TeamUser;
 import com.ssafy.puzzlepop.teamuser.domain.TeamUserRequestDto;
+import com.ssafy.puzzlepop.user.domain.UserDto;
+
+import java.util.List;
 
 public interface TeamUserService {
-    public Long readTeamUser(Long id);
+    public TeamUser readTeamUser(Long id);
     public Long createTeamUser(TeamUserRequestDto requestDto);
     public Long updateTeamUser(TeamUserRequestDto requestDto);
     public void deleteTeamUser(Long id);
 
+    public List<UserDto> findAllByTeamId(Long teamId);
+    public List<TeamDto> findAllByUserId(Long userId);
 }
--- src/main/java/com/ssafy/puzzlepop/teamuser/service/TeamUserServiceImpl.java
+++ src/main/java/com/ssafy/puzzlepop/teamuser/service/TeamUserServiceImpl.java
@@ -1,14 +1,19 @@
 package com.ssafy.puzzlepop.teamuser.service;
 
 import com.ssafy.puzzlepop.team.domain.Team;
+import com.ssafy.puzzlepop.team.domain.TeamDto;
 import com.ssafy.puzzlepop.team.repository.TeamRepository;
-import com.ssafy.puzzlepop.teamuser.domain.TeamUserDto;
+import com.ssafy.puzzlepop.teamuser.domain.TeamUser;
 import com.ssafy.puzzlepop.teamuser.domain.TeamUserRequestDto;
 import com.ssafy.puzzlepop.teamuser.repository.TeamUserRepository;
 import com.ssafy.puzzlepop.user.domain.User;
+import com.ssafy.puzzlepop.user.domain.UserDto;
 import com.ssafy.puzzlepop.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -18,27 +23,56 @@ public class TeamUserServiceImpl implements TeamUserService{
     private final TeamUserRepository teamUserRepository;
 
     @Override
-    public Long readTeamUser(Long id) {
+    @Transactional
+    public TeamUser readTeamUser(Long id) {
+        TeamUser teamuser = teamUserRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("TeamUser Not Found with id: " + id));
+        return null;
+    }
+
+
+    @Override
+    public List<UserDto> findAllByTeamId(Long teamId) {
+        List<TeamUser> teamUsers = teamUserRepository.findAllByTeamId(teamId);
         return null;
     }
 
     @Override
+    public List<TeamDto> findAllByUserId(Long userId) {
+        return null;
+    }
+
+    @Override
+    @Transactional
     public Long createTeamUser(TeamUserRequestDto requestDto) {
         User user = userRepository.findById(requestDto.getUserId()).orElseThrow(
                 () -> new IllegalArgumentException("User Not Found with id: " + requestDto.getUserId()));
         Team team = teamRepository.findById(requestDto.getTeamId()).orElseThrow(
                 () -> new IllegalArgumentException("Team Not Found with id: " + requestDto.getTeamId()));
-
-        return null;
+        TeamUser entity = TeamUser.builder().team(team).user(user).build();
+        return teamUserRepository.save(entity).getId();
     }
 
     @Override
+    @Transactional
     public Long updateTeamUser(TeamUserRequestDto requestDto) {
-        return null;
+        TeamUser entity = teamUserRepository.findById(requestDto.getId()).orElseThrow(
+                () -> new IllegalArgumentException("TeamUser Not Found with id: " + requestDto.getId()));
+        Team team = teamRepository.findById(requestDto.getTeamId()).orElseThrow(
+                () -> new IllegalArgumentException("Team Not Found with id: " + requestDto.getTeamId()));
+        User user = userRepository.findById(requestDto.getUserId()).orElseThrow(
+                () -> new IllegalArgumentException("User Not Found with id: " + requestDto.getUserId()));
+        entity.updateTeam(team);
+        entity.updateUser(user);
+        return teamUserRepository.save(entity).getId();
     }
 
     @Override
+    @Transactional
     public void deleteTeamUser(Long id) {
-
+        TeamUser entity = teamUserRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("TeamUser Not Found with id: " + id));
+        teamUserRepository.delete(entity);
     }
+
 }
