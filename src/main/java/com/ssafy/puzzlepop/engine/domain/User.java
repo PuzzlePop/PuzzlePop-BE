7dcc8e94a2c33f02e771af60e2cb8d351cee824e
dlwndus0728 <dlwndus0728@ajou.ac.kr>
2024-02-05 17:20:00 +0900

    feat: 퍼즐 사진 변경 기능 추가

--- src/main/java/com/ssafy/puzzlepop/engine/domain/User.java
+++ src/main/java/com/ssafy/puzzlepop/engine/domain/User.java
@@ -2,9 +2,11 @@ package com.ssafy.puzzlepop.engine.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class User {
     private String id;
