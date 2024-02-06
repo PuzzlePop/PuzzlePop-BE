83271963656b2ba84ae4651d0ba2abd920d0c32e
dlwndus0728 <dlwndus0728@ajou.ac.kr>
2024-02-06 10:12:56 +0900

    fix: 퍼즐 알고리즘 배열 축소

--- src/main/java/com/ssafy/puzzlepop/engine/service/GameService.java
+++ src/main/java/com/ssafy/puzzlepop/engine/service/GameService.java
@@ -217,12 +217,12 @@ public class GameService {
             for (int i = 0; i < arr.length; i++) {
                 PieceDto now = arr[i];
                 int[] p = ourPuzzle.getIdxToCoordinate().get(now.getIndex());
-                if (ourPuzzle.getBoard()[0][p[0]][p[1]].isLocked()) {
+                if (ourPuzzle.getBoard()[p[0]][p[1]].isLocked()) {
                     res.setMessage("BLOCKED");
                     return res;
                 }
 
-                ourPuzzle.getBoard()[0][p[0]][p[1]].setLocked(true);
+                ourPuzzle.getBoard()[p[0]][p[1]].setLocked(true);
             }
 
             System.out.println(targets + " 피스 잠금");
@@ -235,7 +235,7 @@ public class GameService {
                 PieceDto now = arr[i];
                 int[] p = ourPuzzle.getIdxToCoordinate().get(now.getIndex());
 
-                ourPuzzle.getBoard()[0][p[0]][p[1]].setLocked(false);
+                ourPuzzle.getBoard()[p[0]][p[1]].setLocked(false);
             }
 
             System.out.println(targets + " 피스 잠금 해제");
@@ -250,8 +250,8 @@ public class GameService {
                 PieceDto now = arr[i];
                 int[] p = ourPuzzle.getIdxToCoordinate().get(now.getIndex());
 
-                ourPuzzle.getBoard()[0][p[0]][p[1]].setPosition_x(now.getX());
-                ourPuzzle.getBoard()[0][p[0]][p[1]].setPosition_y(now.getY());
+                ourPuzzle.getBoard()[p[0]][p[1]].setPosition_x(now.getX());
+                ourPuzzle.getBoard()[p[0]][p[1]].setPosition_y(now.getY());
             }
 
             res.setTargets(targets);
