ed117520432205cd7f6563c2e9e400e58e8e2c8d
김한중 <hjhzkc@gmail.com>
2024-02-07 17:37:28 +0900

    fix: Fix return data

--- src/main/java/com/ssafy/puzzlepop/user/controller/LoginController.java
+++ src/main/java/com/ssafy/puzzlepop/user/controller/LoginController.java
@@ -1,14 +1,18 @@
 package com.ssafy.puzzlepop.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
+@CrossOrigin("*")
 public class LoginController {
 
     @GetMapping("/login")
     public String loginPage() {
-
-        return "loginForm";
+        System.out.println("LOGIN");
+        return "redirect:/oauth2/authorization/google";
     }
+
 }
