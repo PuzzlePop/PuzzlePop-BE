3bcca8b1c9f542f7ad092975ef1ee4aef5e5f29f
조시훈 <kepler452k@naver.com>
2024-02-09 15:49:28 +0900

    fix: Remove GlobalException in Specific Handler

--- src/main/java/com/ssafy/puzzlepop/teamuser/advice/TeamUserExceptionHandler.java
+++ src/main/java/com/ssafy/puzzlepop/teamuser/advice/TeamUserExceptionHandler.java
@@ -2,27 +2,26 @@ package com.ssafy.puzzlepop.teamuser.advice;
 
 import com.ssafy.puzzlepop.teamuser.exception.TeamUserNotFoundException;
 import com.ssafy.puzzlepop.teamuser.exception.TeamUserValidationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class TeamUserExceptionHandler {
 
     @ExceptionHandler(TeamUserNotFoundException.class)
     public ResponseEntity<String> handleTeamNotFoundException(TeamUserNotFoundException e) {
+        log.error("TeamUserExceptionHandler: " + e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(TeamUserValidationException.class)
     public ResponseEntity<String> handleTeamValidationException(TeamUserValidationException e) {
+        log.error("TeamUserExceptionHandler: " + e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGenericException(Exception e) {
-        // 예상치 못한 예외에 대한 처리
-        return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
-    }
 }
