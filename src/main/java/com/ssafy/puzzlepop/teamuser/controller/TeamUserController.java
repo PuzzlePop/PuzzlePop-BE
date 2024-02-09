aa7f9d848dfec1786aaf0de54a1a1d2f64944b77
조시훈 <kepler452k@naver.com>
2024-02-09 15:48:12 +0900

    feat: Add log process for checking log in server

--- src/main/java/com/ssafy/puzzlepop/teamuser/controller/TeamUserController.java
+++ src/main/java/com/ssafy/puzzlepop/teamuser/controller/TeamUserController.java
@@ -4,12 +4,14 @@ import com.ssafy.puzzlepop.teamuser.domain.TeamUserRequestDto;
 import com.ssafy.puzzlepop.teamuser.domain.TeamUserResponseDto;
 import com.ssafy.puzzlepop.teamuser.service.TeamUserServiceImpl;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 public class TeamUserController {
@@ -17,12 +19,14 @@ public class TeamUserController {
 
     @GetMapping("/teamuser")
     public ResponseEntity<?> readTeamUser(Long id) {
+        log.info("TeamUserController - readTeamUser(): id = " + id);
         TeamUserResponseDto responseDto = teamUserService.readTeamUser(id);
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @GetMapping("/teamuser/search")
     public ResponseEntity<?> findAllByFilter(@RequestParam String filter, @RequestParam Long id) {
+        log.info("TeamUserController - findAllByFilter(): filter = " + filter + ", id = " + id);
         List<TeamUserResponseDto> responseDtos = switch (filter) {
             case "teamId" -> teamUserService.findAllByTeamId(id);
             case "userId" -> teamUserService.findAllByUserId(id);
@@ -34,18 +38,21 @@ public class TeamUserController {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     @PostMapping("/teamuser")
     public ResponseEntity<?> createTeamUser(@RequestBody TeamUserRequestDto requestDto) {
+        log.info("TeamUserController - createTeamUser(): requestDto = " + requestDto);
         Long responseId = teamUserService.createTeamUser(requestDto);
         return ResponseEntity.status(HttpStatus.OK).body(responseId);
     }
 
     @PutMapping("/teamuser")
     public ResponseEntity<?> updateTeamUser(@RequestBody TeamUserRequestDto requestDto) {
+        log.info("TeamUserController - updateTeamUser(): requestDto = " + requestDto);
         Long responseId = teamUserService.updateTeamUser(requestDto);
         return ResponseEntity.status(HttpStatus.OK).body(responseId);
     }
 
     @DeleteMapping("/teamuser")
     public ResponseEntity<?> deleteTeamUser(@RequestParam Long id) {
+        log.info("TeamUserController - deleteTeamUser(): id = " + id);
         Long responseId = teamUserService.deleteTeamUser(id);
         return ResponseEntity.status(HttpStatus.OK).body(responseId);
     }
