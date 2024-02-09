651aa78cefa41d46bd783011d4ecbd9a9d111872
조시훈 <kepler452k@naver.com>
2024-02-09 17:30:43 +0900

    fix: Fix some function's Parameters - Add log process - Remove duplicated Exception - Remove GlobalException in Specific Handler

--- src/main/java/com/ssafy/puzzlepop/teamitem/controller/TeamItemController.java
+++ src/main/java/com/ssafy/puzzlepop/teamitem/controller/TeamItemController.java
@@ -2,84 +2,59 @@ package com.ssafy.puzzlepop.teamitem.controller;
 
 import com.ssafy.puzzlepop.teamitem.domain.TeamItemRequestDto;
 import com.ssafy.puzzlepop.teamitem.domain.TeamItemResponseDto;
-import com.ssafy.puzzlepop.teamitem.exception.ItemNotFoundException;
-import com.ssafy.puzzlepop.teamitem.exception.TeamItemNotFoundException;
-import com.ssafy.puzzlepop.teamitem.exception.TeamNotFoundException;
-import com.ssafy.puzzlepop.teamitem.service.TeamItemService;
 import com.ssafy.puzzlepop.teamitem.service.TeamItemServiceImpl;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 public class TeamItemController {
     private final TeamItemServiceImpl teamItemService;
 
     @GetMapping("/teamitem")
-    public ResponseEntity<?> getTeamItemById(@RequestParam("id") Long id) {
-        try {
-            TeamItemResponseDto responseDto = teamItemService.getTeamItemById(id);
-            return ResponseEntity.status(HttpStatus.OK).body(responseDto);
-        } catch (TeamNotFoundException | ItemNotFoundException | TeamItemNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-        }
+    public ResponseEntity<?> readTeamItem(@RequestParam Long id) {
+        log.info("TeamItemController - readTeamItem(): id = " + id);
+        TeamItemResponseDto responseDto = teamItemService.readTeamItem(id);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
+    @GetMapping("/teamitem/search")
+    public ResponseEntity<?> findAllByFilter(@RequestParam String filter, @RequestParam Long id) {
+        log.info("TeamItemController - findAllByFilter(): filter = " + filter + ", id = " + id);
+        List<TeamItemResponseDto> responseDtos = switch (filter) {
+            case "teamId" -> teamItemService.findAllByTeamId(id);
+            case "itemId" -> teamItemService.findAllByItemId(id);
+            default -> null;
+        };
+        return ResponseEntity.status(HttpStatus.OK).body(responseDtos);
+    }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     @PostMapping("/teamitem")
     public ResponseEntity<?> createTeamItem(@RequestBody TeamItemRequestDto requestDto) {
-        try {
-            Long teamId = requestDto.getTeamId();
-            Long itemId = requestDto.getItemId();
-            Long id = teamItemService.createTeamItem(teamId, itemId);
-            return ResponseEntity.status(HttpStatus.CREATED).body(id);
-        } catch (TeamNotFoundException | ItemNotFoundException | TeamItemNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-        }
+        log.info("TeamItemController - createTeamItem(): requestDto = " + requestDto);
+        Long responseId = teamItemService.createTeamItem(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseId);
     }
 
     @PutMapping("/teamitem")
-    public ResponseEntity<?> updateTeamItem(@RequestParam("id") Long id,
-                                            @RequestParam("itemId") Long itemId) {
-        try {
-            teamItemService.updateTeamItem(id, itemId);
-            return ResponseEntity.status(HttpStatus.OK).body(id);
-        } catch (TeamNotFoundException | ItemNotFoundException | TeamItemNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-        }
+    public ResponseEntity<?> updateTeamItem(@RequestBody TeamItemRequestDto requestDto) {
+        log.info("TeamItemController - updateTeamItem(): requestDto = " + requestDto);
+        Long responseId = teamItemService.updateTeamItem(requestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(responseId);
     }
 
     @DeleteMapping("/teamitem")
     public ResponseEntity<?> deleteTeamItem(@RequestParam Long id) {
-        try {
-            teamItemService.deleteTeamItem(id);
-            return ResponseEntity.status(HttpStatus.OK).body(id);
-        } catch (TeamNotFoundException | ItemNotFoundException | TeamItemNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-        }
+        log.info("TeamItemController - deleteTeamItem(): id = " + id);
+        Long responseId = teamItemService.deleteTeamItem(id);
+        return ResponseEntity.status(HttpStatus.OK).body(responseId);
     }
 
-    @GetMapping("/teamitem/search")
-    public ResponseEntity<?> getTeamItemsByTeamId(@RequestParam("teamId") Long teamId) {
-        try {
-            List<TeamItemResponseDto> responseDtos = teamItemService.getTeamItemsByTeamId(teamId);
-            return ResponseEntity.status(HttpStatus.OK).body(responseDtos);
-        } catch (TeamNotFoundException | ItemNotFoundException | TeamItemNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-        }
-    }
 }
