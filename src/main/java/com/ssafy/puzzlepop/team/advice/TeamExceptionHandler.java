6f17a2b12e6573fff9ff8974120bbb81c0ab69a4
조시훈 <kepler452k@naver.com>
2024-02-09 16:17:39 +0900

    feat: Add log process Remove GlobalException in Specific Handler

--- src/main/java/com/ssafy/puzzlepop/team/advice/TeamExceptionHandler.java
+++ src/main/java/com/ssafy/puzzlepop/team/advice/TeamExceptionHandler.java
@@ -2,27 +2,26 @@ package com.ssafy.puzzlepop.team.advice;
 
 import com.ssafy.puzzlepop.team.exception.TeamNotFoundException;
 import com.ssafy.puzzlepop.team.exception.TeamValidationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class TeamExceptionHandler {
 
     @ExceptionHandler(TeamNotFoundException.class)
     public ResponseEntity<String> handleTeamNotFoundException(TeamNotFoundException e) {
+        log.error("TeamExceptionHandler: " + e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(TeamValidationException.class)
     public ResponseEntity<String> handleTeamValidationException(TeamValidationException e) {
+        log.error("TeamExceptionHandler: " + e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGenericException(Exception e) {
-        // 예상치 못한 예외에 대한 처리
-        return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
-    }
 }
--- src/main/java/com/ssafy/puzzlepop/team/controller/TeamController.java
+++ src/main/java/com/ssafy/puzzlepop/team/controller/TeamController.java
@@ -4,12 +4,14 @@ import com.ssafy.puzzlepop.team.domain.TeamDto;
 import com.ssafy.puzzlepop.team.exception.TeamNotFoundException;
 import com.ssafy.puzzlepop.team.service.TeamServiceImpl;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 public class TeamController {
@@ -17,12 +19,14 @@ public class TeamController {
 
     @GetMapping("/team")
     public ResponseEntity<?> readTeam(@RequestParam Long id) {
+        log.info("TeamController - readTeam(): id = " + id);
         TeamDto responseDto = teamService.readTeam(id);
         return ResponseEntity.status(HttpStatus.FOUND).body(responseDto);
     }
 
     @GetMapping("/team/all")
     public ResponseEntity<?> readAllTeams() {
+        log.info("TeamController - readAllTeams()");
         List<TeamDto> responseDtos = teamService.readAllTeams();
         return ResponseEntity.status(HttpStatus.FOUND).body(responseDtos);
     }
@@ -30,18 +34,21 @@ public class TeamController {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     @PostMapping("/team")
     public ResponseEntity<?> createTeam(@RequestBody TeamDto requestDto) {
+        log.info("TeamController - createTeam(): requestDto = " + requestDto);
         Long id = teamService.createTeam(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(id);
     }
 
     @PutMapping("/team")
     public ResponseEntity<?> updateTeam(@RequestBody TeamDto requestDto) {
+        log.info("TeamController - updateTeam(): requestDto = " + requestDto);
         Long id = teamService.updateTeam(requestDto);
         return ResponseEntity.status(HttpStatus.OK).body(id);
     }
 
     @DeleteMapping("/team")
     public ResponseEntity<?> deleteTeam(@RequestParam Long id) {
+        log.info("TeamController - deleteTeam(): id = " + id);
         id = teamService.deleteTeam(id);
         return ResponseEntity.status(HttpStatus.OK).body(id);
     }
@@ -49,12 +56,14 @@ public class TeamController {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     @GetMapping("/team/search")
     public ResponseEntity<?> findTeamsByGameId(@RequestParam Long gameId) {
+        log.info("TeamController - findTeamsByGameId(): gameId = " + gameId);
         List<TeamDto> responseDtos = teamService.findAllByGameId(gameId);
         return ResponseEntity.status(HttpStatus.FOUND).body(responseDtos);
     }
 
     @PutMapping("/team/update")
     public ResponseEntity<?> updateMatchedPieceCount(@RequestBody TeamDto requestDto) {
+        log.info("TeamController - updateTeam(): requestDto = " + requestDto);
         Long id = teamService.updateMatchedPieceCount(requestDto);
         return ResponseEntity.status(HttpStatus.OK).body(id);
     }
